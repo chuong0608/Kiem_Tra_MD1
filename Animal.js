62164a85eff85bc9b73e28ae23c4a9ec01901a01
chuong0608 <chuongtep113@gmail.com>
2022-03-31 10:07:52 +0700

    Kiemtra

---
+++ Animal.js
@@ -0,0 +1,37 @@
+class Animal {
+    name
+    weight
+
+    constructor(name, weight) {
+        this.name = name
+        this.weight = weight
+    }
+
+    setName(name) {
+        this.name = name
+    }
+
+    setWeight(wight) {
+        this.weight = wight
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getWeight() {
+        return this.weight
+    }
+
+    toString() {
+        return ` tên: ${this.name}, cân nặng: ${this.weight}`
+    }
+}
+let objAnimal1 = new Animal();
+objAnimal1.setName("Elephant");
+objAnimal1.setWeight(45.6);
+console.log(objAnimal1.toString())
+let objAnimal2 = new Animal("Dog", 10)
+objAnimal2.setName("Mouse")
+console.log(objAnimal2.getName());
+console.log(objAnimal2.getWeight());
