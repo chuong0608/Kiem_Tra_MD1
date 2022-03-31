9b66d7778d3b1b9973526577ae3de313a1fd2c48
chuong0608 <chuongtep113@gmail.com>
2022-03-31 10:12:00 +0700

    kiemtra

--- Animal.js
+++ Animal.js
@@ -1,26 +1,26 @@
 class Animal {
-    name
-    weight
+    name;
+    weight;
 
     constructor(name, weight) {
-        this.name = name
-        this.weight = weight
+        this.name = name;
+        this.weight = weight;
     }
 
     setName(name) {
-        this.name = name
+        this.name = name;
     }
 
     setWeight(wight) {
-        this.weight = wight
+        this.weight = wight;
     }
 
     getName() {
-        return this.name
+        return this.name;
     }
 
     getWeight() {
-        return this.weight
+        return this.weight;
     }
 
     toString() {
